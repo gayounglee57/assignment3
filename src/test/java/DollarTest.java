df022bd3281664e767a1c595deec4d6c6438c36e
gayoung <gayounglee57@gmail.com>
2019-04-08 10:45:18 +1200

    adds tests back

--- src/test/java/DollarTest.java
+++ src/test/java/DollarTest.java
@@ -7,20 +7,20 @@ import org.junit.Test;
 
 public class DollarTest {
 
-//    @Test
-//    public void testDollarDivision() {
-//    // Given
-//        Dollar ten = new Dollar(10);
-//    // When
-//        Dollar result = ten.dividedBy(2);
-//        // Then
-//        assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
-//    }
+    @Test
+    public void testDollarDivision() {
+        // Given
+        Dollar ten = new Dollar(10);
+        // When
+        Dollar result = ten.dividedBy(2);
+        // Then
+        assertEquals(new Dollar(5), result); // You could change 5 to 4 to see the build failure
+    }
 
-//    @Test(expected=ArithmeticException.class) public void testDollarDividedByZero() {
-//        // Given
-//        Dollar ten = new Dollar(10);
-//        // When
-//        ten.dividedBy(0);
-//    }
+    @Test(expected=ArithmeticException.class) public void testDollarDividedByZero() {
+        // Given
+        Dollar ten = new Dollar(10);
+        // When
+        ten.dividedBy(0);
+    }
 }
